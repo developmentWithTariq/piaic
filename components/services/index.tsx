aec176f5b34701c13f6bbae0b39be85b24a50f29
Tariq Nawaz <tariqnawaz15994@gmail.com>
2023-02-25 20:57:16 +0500

    Create index.tsx

---
+++ components/services/index.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { Box, Button, Heading, Text, VStack } from "@chakra-ui/react";
+
+const Services = () => {
+  return (
+    
+    <VStack borderRadius='14' py='2em' m='auto' my='3em' color='green.800' spacing='1em'>
+        <Heading fontSize={{base:'2xl',md:'2xl',lg:'3xl',xl:'4xl'}} fontWeight='bold' textAlign='center' >Getting Ready for the Next Generation of the Internet</Heading>
+        <Text w='90vw' m='auto' fontSize={{base:'sm',sm:'md',lg:'lg',xl:'xl' }}fontWeight='normal'>
+              The internet is without a doubt the most important technological development in human history. Web3, 3D Metaverse, AI, IoT, Cloud, and Edge technologies expand the internet as we know it by introducing novel features and advancements. Metaverse will make use of all aspects of modern technology, including 3D, VR, AR, AI, blockchain, cloud and edge computing, voice computing, ambient computing, and more.
+              In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the program's beginning. It resembles a cross between a corporate venture and an educational project.
+        </Text>
+    </VStack>
+  )
+}
+
+export default Services;
